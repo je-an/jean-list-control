21053998a127b643832b4d2106d46d50609973f8
je-an <mail@jeremiasandris.de>
2018-09-22 19:12:07 +0200

    Add API documentation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Description
 
-Provides functionality for displaying data as a list -> Work in progress
+Provides functionality for displaying data as a list
 
 ## Support
 Supports AMD eco system. If there is no loader, ListControl is registered as a browser variable.
@@ -28,10 +28,17 @@ require(["path/to/ListControl"], function(ListControl){
 **Options**
 - **height**: `Number` - `optional` - height of the list in px
 
+### ListControl.commit() 
 
-### ListControl.set(id, name, description) 
+Appends the until now added rows to DOM
 
-Set an entry to the list
+**Returns**
+- `Boolean` - True if the currently added rows are appended to DOM
+
+
+### ListControl.add(id, name, details) 
+
+Adds an entry to the list
 
 **Parameters**
 - **id**: `String` - id of the list entry
@@ -41,9 +48,60 @@ Set an entry to the list
 **Returns**
 - `Boolean` - True, if the new list entry could be set, otherwise exception
 
+
+### ListControl.update(id, name, details) 
+
+Adds an entry to the list
+
+**Parameters**
+- **id**: `String` - id of the list entry
+- **name**: `String` - name of the list entry
+- **details**: `String` - detail information of the list entry
+
+**Returns**
+- `Boolean` - True, if the new list entry could be set, otherwise exception
+
+
+### ListControl.get() 
+
+Not implemented yet
+
+
+### ListControl.remove(id) 
+
+Removes an entry from the list
+
+**Parameters**
+- **id**: `String` - id of the list entry to be removed
+
+**Returns**
+- `Boolean` - True, if the list entry could be removed, false otherwise
+
+### ListControl.clear() 
+
+Removes all entries from the list
+
+**Returns**
+- `Boolean` - True, if all list entries could be removed, false otherwise
+
+### ListControl.lock() 
+
+Locks the control for input
+
+**Returns**
+- `Boolean` - True if control is locked
+
+### ListControl.unlock() 
+
+Unlocks the control for input
+
+**Returns**
+- `Boolean` - True if control is unlocked
+
 ## Tests
 
 - Open spec/spec-runner.html in browser to see the test cases.
+- Open example/index.html in browser to see the playground.
 
 ## License
 
--- src/ListControl.js
+++ src/ListControl.js
@@ -48,21 +48,20 @@ define([ // jscs:ignore
             COLLAPSED: 0,
             DISPLAYED: 1
         };
-        /** */
+        /** @returns {Boolean} - True if the currently added rows are appended to DOM */
         ListControl.prototype.commit = function () {
             this.body.appendChild(this._listElementFragment);
+            return true;
         };
         /** 
-         * @throws {TypeError} - If id is not a string
-         * @throws {TypeError} - If name is not a string
-         * @throws {TypeError} - If details is not a string
+         * @throws {TypeError} - One or more values are invalid
          * @param {String} id - the id of the list element
          * @param {String} name - the name of the list element
          * @param {String} details - detail information of the list element
          */
         ListControl.prototype.add = function (id, name, details) {
             if (!this._areValuesValid(id, name, details)) {
-                Failure.throwTypeError("provided values are invalid");
+                Failure.throwTypeError("One or more values are invalid");
             }
             var listElement = this._listElement, entry = listElement.cloneNode(true),
                 fragment = this._listElementFragment;
@@ -71,7 +70,12 @@ define([ // jscs:ignore
             fragment.appendChild(entry);
             return true;
         };
-        /** */
+        /** 
+         * @throws {TypeError} - One or more values are invalid
+         * @param {String} id - the id of the list element
+         * @param {String} name - the name of the list element
+         * @param {String} details - detail information of the list element
+         */
         ListControl.prototype.update = function (id, name, details) {
             if (!this._areValuesValid(id, name, details)) {
                 Failure.throwTypeError("provided values are invalid");
@@ -83,32 +87,53 @@ define([ // jscs:ignore
         ListControl.prototype.get = function () {
 
         };
-        /** */
-        ListControl.prototype.remove = function () {
-
+        /** 
+         * @param {String} id - id of the entry to be removed
+         * @returns {Boolean} - True if entry is removed, false if not or if there is no entry;
+         */
+        ListControl.prototype.remove = function (id) {
+            var isRemoved = false, childToBeRemoved = DomUtil.getChildById(this.body, id);
+            if (TypeCheck.isDefined(childToBeRemoved)) {
+                isRemoved = true;
+                this.body.removeChild(childToBeRemoved);
+            }
+            return isRemoved;
         };
-        /** */
+        /** @returns {Boolean} - True if all entries are removed */
         ListControl.prototype.clear = function () {
-
+            var body = this.body;
+            while (body.firstChild) {
+                body.removeChild(body.firstChild);
+            }
+            return true;
         };
-        /** */
+        /** @returns {Boolean} - True if control is locked */
         ListControl.prototype.lock = function () {
-
+            return false;
         };
-        /** */
+        /** @returns {Boolean} - True if control is unlocked */
         ListControl.prototype.unlock = function () {
-
+            return false;
         };
-        /** */
-        ListControl.prototype._setEntryValue = function (node, name, details) {
-            DomUtil.getChildByClass(node, "name").innerHTML = name;
-            DomUtil.getChildByClass(node, "details").innerHTML = details;
+        /** 
+         * @param {HTMLElement} entry - the entry node
+         * @param {String} name - the name of the list element
+         * @param {String} details - detail information of the list element
+         */
+        ListControl.prototype._setEntryValue = function (entry, name, details) {
+            DomUtil.getChildByClass(entry, "name").innerHTML = name;
+            DomUtil.getChildByClass(entry, "details").innerHTML = details;
         }
-        /** */
+        /** 
+         * @param {String} id - the id of the list element
+         * @param {String} name - the name of the list element
+         * @param {String} details - detail information of the list element
+         * @returns {Boolean} - True if values are valid, false otherwise
+         */
         ListControl.prototype._areValuesValid = function (id, name, details) {
             return TypeCheck.isString(id) && TypeCheck.isString(name) && TypeCheck.isString(details);
         };
-        /** */
+        /** @param {Object} e - event object */
         ListControl.prototype._onEntryClick = function (e) {
             var entry = DomUtil.getAncestorByClass(e.target, "jean-list-element"),
                 value = entry.getAttribute("data-state"),
